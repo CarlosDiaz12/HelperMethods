e0164b39fb8410b7c0d98e9b619631972d894e08
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-07 09:54:41 -0400

    page 593

--- Models/Person.cs
+++ Models/Person.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace HelperMethods.Models
 {
     public class Person
     {
+        /*
+         * EXCLUDE PROPERTY FROM SCAFFOLDING
+        [ScaffoldColumn(false)]
+        */
+        [HiddenInput(DisplayValue = false)]
         public int PersonId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
